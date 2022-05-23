f616197c64658f481d0e9768e649e87d5787aa79
Arvind Chopade <arvind.chopade.it@gmail.com>
2022-05-23 09:21:44 +0530

    commit inital

---
+++ Entities/Models/Address.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Models
+{
+    public class Address
+    {
+        public string street { get; set; }
+        public string suite { get; set; }
+        public string city { get; set; }
+        public string zipcode { get; set; }
+        public GEO geo { get; set; }
+    }
+}
